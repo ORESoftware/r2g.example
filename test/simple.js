5667a4302e63cd99811775cff25a0cc2ff71d61f
Olegzandr VD <alex@oresoftware.com>
2018-08-16 12:13:44 -0700

    pdev:'set'

---
+++ test/simple.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const cp = require('child_process');
+const path  = require('path');
+const fs  = require('fs');
+const http  = require('http');
+const assert  = require('assert');
+const EE  = require('events');
+const strm = require('stream');
+
+
+const example = require('../dist');
+
